4109707e49c4d382eaa78e47511e99c7b82479d9
Mars Lee <285753421@qq.com>
2017-12-04 16:00:42 +0800

    cluster 踩坑记

---
+++ redisClusterDocker/cluster/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "6380"
+cd /redis/6380/redis/conf && redis-server redis.conf
+echo "6381"
+cd /redis/6381/redis/conf && redis-server redis.conf
+echo "6382"
+cd /redis/6382/redis/conf && redis-server redis.conf
